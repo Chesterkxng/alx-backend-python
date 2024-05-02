823bda76a79d6bc21879c48ad52781ec7894f8df
Chesterkxng <cgoita00@gmail.com>
2024-05-02 21:55:24 +0000

    advanced

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Task 10.
+'''
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''Retrieves the first element  if exists.
+    '''
+    if lst:
+        return lst[0]
+    else:
+        return None
