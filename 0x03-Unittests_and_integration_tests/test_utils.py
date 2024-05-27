0461bfdc9b86dd85af9bd14f46a1ff3fa17e6d4c
Chesterkxng <cgoita00@gmail.com>
2024-05-27 10:36:19 +0000

    all in one when the same method have to be tested

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+testing utils function
+"""
+from parameterized import parameterized
+import unittest
+from utils import access_nested_map, get_json, memoize
+from unittest.mock import patch, Mock
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    class for testing access_nested_map function
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+
+    def test_access_nested_map(self, nestedMap, path, expected):
+        """
+        accessing nested map test case
+        """
+        self.assertEqual(access_nested_map(nestedMap, path), expected)
